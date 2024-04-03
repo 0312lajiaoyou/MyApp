4365c42fe5eef666823015c68503adff52bcc67e
lalajiaoyou <1347697869@qq.com>
2024-04-03 22:45:38 +0800

    提交计分器（双队伍版本）

--- app/src/main/java/com/example/myapplication0320/MainActivity2.java
+++ app/src/main/java/com/example/myapplication0320/MainActivity2.java
@@ -18,22 +18,29 @@ public class MainActivity2 extends AppCompatActivity {
 
     }
     public void Click(View view) {
-        TextView scoreA=findViewById(R.id.out);
-        int score_old=Integer.parseInt(scoreA.getText().toString());
-        if(view.getId()==R.id.button3){
-            //+3
-            scoreA.setText(String.valueOf(score_old+3));
-        } else if (view.getId()==R.id.button2) {
-            //+2
-            scoreA.setText(String.valueOf(score_old+2));
-        }else if (view.getId()==R.id.button1) {
-            //+1
-            scoreA.setText(String.valueOf(score_old+1));
+        TextView scoreA=findViewById(R.id.out1);
+        TextView scoreB=findViewById(R.id.out2);
+        int score_oldA=Integer.parseInt(scoreA.getText().toString());
+        int score_oldB=Integer.parseInt(scoreB.getText().toString());
+        if(view.getId()==R.id.button3){//+3
+            scoreA.setText(String.valueOf(score_oldA+3));
+        } else if (view.getId()==R.id.button2) {//+2
+            scoreA.setText(String.valueOf(score_oldA+2));
+        }else if (view.getId()==R.id.button1) {//+1
+            scoreA.setText(String.valueOf(score_oldA+1));
+        }else if(view.getId()==R.id.button6){//+3
+            scoreB.setText(String.valueOf(score_oldB+3));
+        } else if (view.getId()==R.id.button5) {//+2
+            scoreB.setText(String.valueOf(score_oldB+2));
+        }else if (view.getId()==R.id.button4) {//+1
+            scoreB.setText(String.valueOf(score_oldB+1));
         }
     }
     public void reset(View view) {
-        TextView scoreA=findViewById(R.id.out);
+        TextView scoreA=findViewById(R.id.out1);
+        TextView scoreB=findViewById(R.id.out2);
         int sum=0;
         scoreA.setText(String.valueOf(sum));
+        scoreB.setText(String.valueOf(sum));
     }
 }
