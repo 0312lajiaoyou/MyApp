3455f82f055972e7166f512e18f75c34153b6309
lalajiaoyou <1347697869@qq.com>
2024-04-10 22:13:59 +0800

    提交货币换算器及汇率更新页面

---
+++ app/src/main/java/com/example/myapplication0320/ConfigActivity.java
@@ -0,0 +1,39 @@
+package com.example.myapplication0320;
+
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+public class ConfigActivity extends AppCompatActivity {
+    Float dollarRate2;
+    Float euroRate2;
+    Float wonRate2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config);
+    }
+
+    public void save(View view) {
+        Intent intent1 = new Intent(this,MainActivity3.class);
+
+        EditText dollarInput = findViewById(R.id.dollar);
+        EditText euroInput = findViewById(R.id.euro);
+        EditText wonInput = findViewById(R.id.won);
+        dollarRate2 =Float.valueOf(dollarInput.getText().toString());
+        euroRate2 =Float.valueOf(euroInput.getText().toString());
+        wonRate2 =Float.valueOf(wonInput.getText().toString());
+        //传递参数
+        intent1.putExtra("dollar_key",dollarRate2);
+        intent1.putExtra("euro_key",euroRate2);
+        intent1.putExtra("won_key",wonRate2);
+
+        startActivity(intent1);
+    }
+}
--- app/src/main/java/com/example/myapplication0320/MainActivity3.java
+++ app/src/main/java/com/example/myapplication0320/MainActivity3.java
@@ -1,21 +1,43 @@
 package com.example.myapplication0320;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity3 extends AppCompatActivity {
-
+    float dollarRate;
+    float euroRate;
+    float wonRate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        //接收传入数据
+        Intent intent = getIntent();
+        dollarRate = intent.getFloatExtra("dollar_key",7.24f);
+        euroRate = intent.getFloatExtra("euro_key",7.80f);
+        wonRate = intent.getFloatExtra("won_key",0.0054f);
+        Log.i(TAG,"onCreate: get dollar2="+dollarRate);
+        Log.i(TAG,"onCreate: get euro2="+euroRate);
+        Log.i(TAG,"onCreate: get won2="+wonRate);
+
+        //显示在控件中
+        //dollarEdit = findViewById(R.id.dollar_edit);
+        //euroEdit = findViewById(R.id.euro_edit);
+        //wonEdit = findViewById(R.id.won_edit);
+        //dollarEdit.setText(String.valueOf(dollarRate));
+        //euroEdit.setText(String.valueOf(euroRate));
+        //wonEdit.setText(String.valueOf(wonRate));
     }
 
     public void Click(View view) {
@@ -38,15 +60,20 @@ public class MainActivity3 extends AppCompatActivity {
             double rmbResult= 0f;
             //判断用户按钮
             if (view.getId()==R.id.button1){
-                rmbResult = input / 7.24;
+                rmbResult = input / dollarRate;
             }else if(view.getId()==R.id.button2){
-                rmbResult = input / 7.80;
+                rmbResult = input / euroRate;
             }else if(view.getId()==R.id.button3){
-                rmbResult = input / 0.0054;
+                rmbResult = input / wonRate;
             }
             TextView result=findViewById(R.id.out);
             result.setText(String.format("%.2f",rmbResult));
         }
+    }
+    public void Config(View view) {
+        //open activity
+        Intent intent = new Intent(this,ConfigActivity.class);
+        startActivity(intent);
     }
 
 }
